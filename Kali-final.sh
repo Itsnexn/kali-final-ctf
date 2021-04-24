1a5a24043525415b09f94a18cb850132263c81b0
itsnexn <itsnexn@gmail.com>
2021-04-25 01:45:05 +0430

    Add some comment to make code better  and add bsdgames

--- Kali-final.sh
+++ Kali-final.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #
 # This script is made only for KaliLinux
+# I use this to make my vm setup easyer for ctf or some pentest stuff...
+# because i use this personaly you may want to change some settings feel 
+# free to add your tools =)
 # 
 # Shout-out to @_superhero1 # I copy pase some of code from him :) 
-# Copyright (c) 2021 (@itsnexn contact me in [instagram, twitter, mail, github])
+# Copyright (c) 2021 (@itsnexn contact me in [instagram, twitter, github])
 ################################################################################
 
 clear
@@ -38,14 +41,21 @@ apt-get update -qq > /dev/null
 # Install kali-everything
 echo -e "${GREEN}Installing kali-everything packages... ${NOCOLOR}"
 echo
+
+# Get and install all of the kali tools and update all tools
 apt-get install -qq -y kali-everything > /dev/null
+ 
 apt-get install -qq -y jq > /dev/null
+
+# Tool for steganography
 apt-get install -qq -y steghide > /dev/null
 apt-get install -qq -y stegseek > /dev/null
 apt-get install -qq -y stegcracker > /dev/null
+
+# tool for check exif data from image
 apt-get install -qq -y exif > /dev/null
 apt-get install -qq -y exiftool > /dev/null
-
+apt-get install -qq -y bsdgames > /dev/null
 
 # Install sublime text
 echo -e "${GREEN}Installing SublimeText... ${NOCOLOR}"
@@ -57,16 +67,22 @@ apt-get update -qq > /dev/null
 apt-get install -qq -y sublime-text > /dev/null
 
 # Install seclists
+# sec lists is a wordlist for security
+# https://github.com/danielmiessler/SecLists
 echo -e "${GREEN}Installing SecLists... ${NOCOLOR}"
 echo
 apt install -qq -y seclists > /dev/null
 
 # Install Ffuf
+# ffuf is a great web fuzzer
+# https://github.com/ffuf/ffuf
 echo -e "${GREEN}Installing Ffuf... ${NOCOLOR}"
 echo
 apt install -qq -y ffuf > /dev/null
 
 # Install oh-my-zsh
+# make a shell better
+# https://github.com/ohmyzsh/ohmyzsh
 echo -e "${GREEN}Configure oh-my-zsh... ${NOCOLOR}"
 echo
 mkdir /opt/github 
@@ -76,23 +92,26 @@ cp /opt/github/itsnexn-settings/zsh/R3verse.zsh-theme ~/.oh-my-zsh/themes
 cat /opt/github/itsnexn-settings/zsh/R3verse.zsh-theme > ~/.zshrc
 
 # Configure vim theme
+# it works well for me
 echo -e "${GREEN}Configure Vim theme... ${NOCOLOR}"
 echo
 mkdir -p ~/.vim/pack/themes/start
 cd ~/.vim/pack/themes/start
 git clone -q https://github.com/dracula/vim.git dracula
 touch ~/.vimrc
-echo 'packadd! dracula' > ~/.vimrc
-echo 'syntax enable' > ~/.vimrc
-echo 'colorscheme dracula' > ~/.vimrc
+echo 'packadd! dracula' >> ~/.vimrc
+echo 'syntax enable' >> ~/.vimrc
+echo 'colorscheme dracula' >> ~/.vimrc
 
 # Add some git settings
+# enable git cache
 echo -e "${GREEN}Adding Git cache...${NOCOLOR}"
 echo
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=3600'
 
 # Configure tmux config
+# Add keyboard shortcuts and some settings
 echo -e "${GREEN}Configure Tmux... ${NOCOLOR}"
 echo
 git clone -q https://github.com/Itsnexn/itsnexn-settings.git /opt/github
@@ -102,15 +121,32 @@ cp /opt/github/itsnexn-settings/tmux/.tmux.conf ~/
 echo -e "${GREEN}Grabbing useful scripts... ${NOCOLOR}"
 echo
 cd /opt/
+
+# git research tool
 git clone -q https://github.com/internetwache/GitTools.git 
+
+# unprivileged Linux process snooping
 git clone -q https://github.com/DominicBreuker/pspy.git
+
+# PentestMonkey PHP R3verse shell
 git clone -q https://github.com/pentestmonkey/php-reverse-shell.git
+
+# OSINT tool on Instagram to collect, analyze, and run reconnaissance.
 git clone -q https://github.com/Datalux/Osintgram.git
+
 git clone -q https://github.com/rebootuser/LinEnum.git
+
+# Linpeas sound familiar right???
 git clone -q https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git
-git clone -q https://github.com/suljot/shellphish.git
+
+# Phishing tool
+# git clone -q https://github.com/suljot/shellphish.git
+
+# John the ripper
 git clone -q https://github.com/openwall/john.git
 mv john johnTheRipper
+
+# a java steganography tool
 wget http://www.caesum.com/handbook/Stegsolve.jar -O stegsolve.jar
 chmod +x stegsolve.jar
 
